98c31fddb30681f34fb93a43f25ad7c832ab7a90
Daniel Klotz <dklotz@goc.realcore-it.de>
2019-04-11 06:37:15 +0200

    Added the sideDrawer open and close effect

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Aux from '../../../hoc/Auxiliary';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0' 
-            }}>
-            {props.children}
-        </div>
-    </Aux>    
-);
+class Modal extends Component {
 
-export default modal; 
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div 
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0' 
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
+    
+
+export default Modal; 
--- src/components/UI/Toolbar/Toolbar.js
+++ src/components/UI/Toolbar/Toolbar.js
@@ -2,15 +2,16 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../UI/Logo/Logo';
 import NavigationItems from '../Navigation/NavigationItems/NavigationItems';
+import DrawerToggle from '../Navigation/SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
 
     <header className={classes.Toolbar}>
-        <div>Menu</div>
+        <DrawerToggle clicked={props.drawerToggleClicked}/>
         <div className={classes.Logo}>
             <Logo></Logo>
         </div>
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems></NavigationItems>
         </nav>
     </header>
