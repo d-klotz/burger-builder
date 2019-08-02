f5d334b0ed2a1ea62b66e9e93bf24d4c4b149264
Daniel Klotz <dklotz@goc.realcore-it.de>
2019-08-02 06:57:11 +0200

    Added more jest tests

--- src/components/UI/Navigation/NavigationItems/navitagationItems.test.js
+++ src/components/UI/Navigation/NavigationItems/navitagationItems.test.js
@@ -8,8 +8,23 @@ import NavigationItem from '../NavigationItem/NavigationItem';
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
-    it('should render two <NavigationItem /> elements if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems />);
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    });
+
+    it('should render two <NavigationItem /> elements if not authenticated', () => {        
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should render three <NavigationItem /> elements if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+
+    it('should render Logout <NavigationItem /> if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link='/logout'>Logout</NavigationItem>)).toEqual(true);
     });
 });
