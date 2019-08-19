9b45b693af4e931a04991b7e32dcac775107eeff
Daniel Klotz <dklotz@goc.realcore-it.de>
2019-08-19 06:06:00 +0200

    Added test for the auth redux

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,35 @@
+import reducer from './auth';
+import * as actions from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        });
+    });
+
+    it('should store the token upon login', () => {
+        expect(reducer({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        }, {
+            type: actions.AUTH_SUCCESS,
+            idToken: 'some-token', 
+            userId: 'some-user-id'
+        })).toEqual({
+            token: 'some-token',
+            userId: 'some-user-id',
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        });
+    });
+});
